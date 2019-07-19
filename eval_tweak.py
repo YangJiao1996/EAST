a6caecef6a1eb1a551fa05b609b611b2bbe54118
YangJiao1996 <kjcjwj007@gmail.com>
2019-07-19 14:49:43 -0400

    Moved functions to eval_utils

--- eval_tweak.py
+++ eval_tweak.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 import locality_aware_nms as nms_locality
 import lanms
 import time
+import eval_utils
 
 import model
 from icdar import restore_rectangle, generate_rbox, load_annoataion, check_and_validate_polys
@@ -19,70 +20,6 @@ tf.app.flags.DEFINE_string('test_path', '', '')
 tf.app.flags.DEFINE_integer('num_threads', 4, 'Number of CPU threads to use')
 
 FLAGS = tf.app.flags.FLAGS
-
-def get_images():
-    '''
-    find image files in test data path
-    :return: list of files found
-    '''
-    files = []
-    exts = ['jpg', 'png', 'jpeg', 'JPG', 'bmp', 'BMP']
-    for parent, dirnames, filenames in os.walk(FLAGS.test_path):
-        for filename in filenames:
-            for ext in exts:
-                if filename.endswith(ext):
-                    files.append(os.path.join(parent, filename))
-                    break
-    print('Find {} images'.format(len(files)))
-    return files
-
-def show_pairs(image_output, image_gt, save_name):
-    fig, axes = plt.subplots(1, 2)
-    axes[0].imshow(image_output, cmap='gray')
-    axes[1].imshow(image_gt, cmap='gray')
-    plt.savefig(save_name)
-    plt.close()
-
-def show_single(image, save_name):
-    plt.imshow(image, cmap='gray')
-    plt.savefig(save_name)
-    plt.close()
-
-def resize_image(im, max_side_len=2400):
-    '''
-    resize image to a size multiple of 32 which is required by the network
-    :param im: the resized image
-    :param max_side_len: limit of max image size to avoid out of memory in gpu
-    :return: the resized image and the resize ratio
-    '''
-    if len(im.shape) == 3:
-        h, w, _ = im.shape
-    else:
-        h, w = im.shape
-
-    resize_w = w
-    resize_h = h
-
-    # limit the max side
-    if max(resize_h, resize_w) > max_side_len:
-        ratio = float(max_side_len) / resize_h if resize_h > resize_w else float(max_side_len) / resize_w
-    else:
-        ratio = 1.
-    resize_h = int(resize_h * ratio)
-    resize_w = int(resize_w * ratio)
-
-    resize_h = resize_h if resize_h % 32 == 0 else (resize_h // 32 - 1) * 32
-    resize_w = resize_w if resize_w % 32 == 0 else (resize_w // 32 - 1) * 32
-    resize_h = max(32, resize_h)
-    resize_w = max(32, resize_w)
-    im = cv2.resize(im, (int(resize_w), int(resize_h)))
-
-    ratio_h = resize_h / float(h)
-    ratio_w = resize_w / float(w)
-
-    return im, (ratio_h, ratio_w)
-
-
 
 def main(argv=None):
     if FLAGS.use_gpu:
@@ -106,10 +43,8 @@ def main(argv=None):
         model_path = os.path.join(FLAGS.checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
         print('Restore from {}'.format(model_path))
         saver.restore(sess, model_path)
-        im_fn_list = get_images()
+        im_fn_list = eval_utils.get_images(FLAGS.test_path)
         for im_fn in im_fn_list:
-            # im_fn = FLAGS.image_file
-
             print(f"Processing {os.path.basename(im_fn)}...")
             imread_start = time.time()
             im = cv2.imread(im_fn)
@@ -125,7 +60,7 @@ def main(argv=None):
                 text_polys, text_tags = check_and_validate_polys(text_polys, text_tags, (im_h, im_w))
                 score_map_gt, geo_map_gt, training_mask_gt = generate_rbox((im_h, im_w), text_polys, text_tags)
 
-            im_resized, (ratio_h, ratio_w) = resize_image(im, max_side_len=384)
+            im_resized, (ratio_h, ratio_w) = eval_utils.resize_image(im, max_side_len=384)
             
             network_fw_start = time.time()
             score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
@@ -133,7 +68,7 @@ def main(argv=None):
             network_fw_time = (network_fw_end - network_fw_start) * 1000
             print(f"Time elapsed in imread(): {imread_time:.2f}ms; network forward time {network_fw_time:.2f}ms.")
             
-            # boxes = detect(score_map=score, geo_map=geometry)
+            boxes = eval_utils.detect(score_map=score, geo_map=geometry)
             if len(score.shape) == 4:
                 score = score[0, :, :, 0]
                 geometry = geometry[0, :, :, ]
@@ -152,9 +87,9 @@ def main(argv=None):
                 os.makedirs(score_path)
             score_file = os.path.join(score_path, score_name)
             if pair_flag:
-                show_pairs(score_map_gt, score_filtered, score_file)
+                eval_utils.show_pairs(score_map_gt, score_filtered, score_file)
             else:
-                show_single(score_filtered, score_file)
+                eval_utils.show_single(score_filtered, score_file)
             for idx in range(5):
                 geo_name = im_fn_base + "_geo" + str(idx) + ".png"
                 geo_path = os.path.join(FLAGS.output_dir, "geometries")
@@ -162,11 +97,17 @@ def main(argv=None):
                     os.makedirs(geo_path)
                 geo_file = os.path.join(geo_path, geo_name)
                 if pair_flag:
-                    show_pairs(geo_map_gt[:, :, idx], geometry_filtered[:, :, idx], geo_file)
+                    eval_utils.show_pairs(geo_map_gt[:, :, idx], geometry_filtered[:, :, idx], geo_file)
                 else:
-                    show_single(geometry_filtered[:, :, idx], geo_file)
+                    eval_utils.show_single(geometry_filtered[:, :, idx], geo_file)
 
-        
+            if boxes is not None:
+                res_path = os.path.join(FLAGS.output_dir, "boxes")
+                res_name = im_fn_base + "_box.png"
+                res_file = os.path.join(res_path, res_name)
+                if not os.path.exists(res_path):
+                    os.makedirs(res_path)
+                eval_utils.show_polygons(im_resized, boxes, res_file)
 
 
 
